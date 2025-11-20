60deee815393c41f49253d5f4c6cac587dd8fc49
helen_alimova <shchurova93@bk.ru>
2025-11-21 00:08:00 +0300

    Создан пайплайн DVC для подготовки данных и обучения модели

---
+++ src/prepare.py
@@ -0,0 +1,15 @@
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def main():
+    # Загрузка данных
+    data = pd.read_csv('C:\Users\Констатнтин\my_ml_project\data\raw')  # Предполагаем, что есть raw данные
+    # Сплит на train/test
+    train, test = train_test_split(data, test_size=0.2, random_state=42)
+    # Сохранение обработанных данных
+    train.to_csv('data/processed/train.csv', index=False)
+    test.to_csv('data/processed/test.csv', index=False)
+
+if __name__ == '__main__':
+    main()
